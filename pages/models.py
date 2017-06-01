eef79c32282134573d90ad97cafe19d2e070e4f1
ssanaul <ss7@illinois.edu>
2017-06-01 03:14:56 -0500

    many to many rel. user and pages

--- pages/models.py
+++ pages/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Page(models.Model):
 	def __str__(self):
 		return "\nTitle: "+self.title+"\nDescription:\n"+self.description
+		
 	#page_id = unique id...
+	
+	users = models.ManyToManyField(User,
+		related_name="pages")
+	
 	title = models.CharField(max_length=30)
 	description = models.CharField(max_length=200)
 	htmlHead = models.TextField()
