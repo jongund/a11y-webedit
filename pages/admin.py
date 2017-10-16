22789180453998f82eaa4b0d86bbeb23bdb81480
Jon Gunderson <jongund@illinois.edu>
2017-10-16 14:14:02 -0500

    added a way to populate tags and added admin to pages models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Page
+from .models import Tag
+
 # Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'lastUpdated')
+
+admin.site.register(Page, PageAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+
+admin.site.register(Tag, TagAdmin)
