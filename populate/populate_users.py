177f90e7b34ca80fa381f072879d25d80954667f
Jon Gunderson <jongund@illinois.edu>
2017-10-16 13:21:11 -0500

    moved populate script to populate and added config information

---
+++ populate/populate_users.py
@@ -0,0 +1,41 @@
+import django
+import json
+import sys
+import os
+
+fp = os.path.realpath(__file__)
+path, filename = os.path.split(fp)
+
+webedit_path = path.split('/populate')[0]
+sys.path.append(webedit_path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WebEdit.settings")
+
+from django.conf import settings
+
+django.setup()
+
+from django.core.exceptions 	import ObjectDoesNotExist
+from django.contrib.auth.models import User
+
+users = (
+	(settings.ADMIN_USERNAME, settings.ADMIN_PASSWORD, True, True),
+)
+
+def create_users(users):
+
+	for person in users:
+
+		try:
+			print("Update User: " + person[0]),
+			user = User.objects.get(username=person[0])
+			user.is_superuser = person[2]
+			user.is_staff 	  = person[3]
+
+		except ObjectDoesNotExist:
+			print("Create User: " + person[0])
+			user = User(username=person[0], is_superuser=person[2], is_staff=person[3])
+
+		user.save()
+
+create_users(users)
