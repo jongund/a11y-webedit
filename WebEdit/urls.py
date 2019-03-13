02772857aa728cb91870866d269c3d4cb12d231a
Mihir Kumar <mkumar12@illinois.edu>
2019-03-13 13:46:13 -0500

    Samples missing.

--- WebEdit/urls.py
+++ WebEdit/urls.py
@@ -20,6 +20,7 @@ import django.contrib.auth.urls
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
+from django.contrib.auth import views as auth_views
 
 from accounts.views import ShibbolethLogin
 from accounts.views import ShibbolethLogout
@@ -31,10 +32,14 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 #	url(r'^$', lambda r: HttpResponseRedirect('pages/new')),
 	url(r'^', include('pages.urls')),
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^accounts/', include('django_registration.backends.activation.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^reset/',    include('password_reset.urls')),
+    # url(r'^accounts/password_reset/', auth_views.PasswordChangeView.as_view()),
+    # url(r'^accounts/password_reset/done', auth_views.PasswordChangeDoneView.as_view()),
+    # url(r'^reset/',    include('password_reset.urls')),
     url(r'^profile/',  include('accounts.urls')),
+
+    # url(r'^login$', auth_views.LoginView, name='login'),
 #    url(r'^login/$',                ShibbolethLogin.as_view(),       name='shib_login'),
 #    url(r'^logout/$',               ShibbolethLogout.as_view(),      name='shib_logout'),
 #    url(r'^discovery/$',            ShibbolethDiscovery.as_view(),   name='shib_discovery'),
