cf52a5bcb9b125c9933212541a9983f32812731f
Adarsh Agarwal <agarwaladarsh95@gmail.com>
2019-09-19 18:42:43 -0500

    added secrets_template file for reference

--- populate/populate_users.py
+++ populate/populate_users.py
@@ -15,42 +15,46 @@ from django.conf import settings
 
 django.setup()
 
-from django.core.exceptions      import ObjectDoesNotExist
-from django.core.exceptions      import ImproperlyConfigured
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ImproperlyConfigured
 from django.contrib.sites.models import Site
-from django.contrib.auth.models  import User
+from django.contrib.auth.models import User
 
 users = (
-	(settings.ADMIN_USERNAME, settings.ADMIN_PASSWORD, settings.ADMIN_EMAIL, settings.ADMIN_FIRST_NAME, settings.ADMIN_LAST_NAME, True, True),
+    (settings.ADMIN_USERNAME, settings.ADMIN_PASSWORD, settings.ADMIN_EMAIL, settings.ADMIN_FIRST_NAME,
+     settings.ADMIN_LAST_NAME, True, True),
 )
 
+
 def create_users(users):
+    for person in users:
 
-  for person in users:
-
-    try:
-      print("Update User: " + person[0])
-      user = User.objects.get(username=person[0])
-      user.email        = person[2]
-      user.first        = person[3]
-      user.last         = person[4]
-      user.is_superuser = person[5]
-      user.is_staff 	  = person[6]
-      user.set_password(person[1])
+        try:
+            print("Update User: " + person[0])
+            user = User.objects.get(username=person[0])
+            user.email = person[2]
+            user.first = person[3]
+            user.last = person[4]
+            user.is_superuser = person[5]
+            user.is_staff = person[6]
+            user.set_password(person[1])
 
 
-    except ObjectDoesNotExist:
-      print("Create User: " + person[0])
-      user = User(username=person[0], email=person[2], first=person[3], last=person[4], is_superuser=person[5], is_staff=person[6])
-      user.set_password(person[1])
+        except ObjectDoesNotExist:
+            print("Create User: " + person[0])
+            user = User(username=person[0], email=person[2], first_name=person[3], last_name=person[4], is_superuser=person[5],
+                        is_staff=person[6])
+            user.set_password(person[1])
 
-    user.save()
+        user.save()
+
 
 def set_site(name, url):
     site = Site.objects.get_current()
     site.domain = url
     site.name = name
     site.save()
+
 
 create_users(users)
 set_site(settings.SITE_NAME, settings.SITE_URL)
